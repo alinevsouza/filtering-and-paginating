1d61d377a865011252e684b4f716f10d8ef1139d
alinevsouza <alinevitoriadesouza0@gmail.com>
2025-06-25 10:43:13 -0300

    Add useEffect import to SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react'; // Adicionando importação do useEffect
 import PropTypes from 'prop-types';
 import useDebounce from '../hooks/useDebounce';
 
